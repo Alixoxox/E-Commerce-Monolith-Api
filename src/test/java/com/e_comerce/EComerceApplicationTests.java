f1b1b97227a1c4832aee60d70b881019e717967d
Alixoxox <Alixoxox@users.noreply.github.com>
2026-08-26 22:23:07 +0500

    fixed the unit mock dependency bugs

--- src/test/java/com/e_comerce/EComerceApplicationTests.java
+++ src/test/java/com/e_comerce/EComerceApplicationTests.java
@@ -1,12 +1,21 @@
 package com.e_comerce;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
-@ActiveProfiles("local")
+@ActiveProfiles("test")
 class EComerceApplicationTests {
+
+	@MockitoBean
+    private RabbitTemplate rabbitTemplate;
+
+	@MockitoBean
+    private ConnectionFactory connectionFactory;
 
 	@Test
 	void contextLoads() {
